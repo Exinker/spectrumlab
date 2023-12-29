3ccf91dc8cc994b41cad501d764be263839ac17e
Exinker <exinker@gmail.com>
2023-12-29 11:19:17 +0700

    tests: add flip and integral test

--- spectrumlab/emulation/curve.py
+++ spectrumlab/emulation/curve.py
@@ -11,8 +11,8 @@ def gauss(x: float | Array[float], x0: float, w: float) -> Array[float]:
     """Gauss (or normal) distribution with position `x0` and unit intensity.
 
     Params:
-        x0: float - position;
-        w: float - width.
+        x0: float - position
+        w: float - width
     """
 
     F = np.exp( -(1/2)*((x - x0) / w)**2 ) / ( np.sqrt(2*np.pi) * w )
@@ -24,8 +24,8 @@ def lanczos(x: float | Array[float], x0: float, a: int = 2) -> Array[float]:
     """Lanczos distribution with position `x0`.
     
     Params:
-        x0: float - position;
-        a: float - window width.
+        x0: float - position
+        a: float - window width
     """
 
     F = np.sinc(x - x0) * np.sinc((x - x0) / a)
@@ -38,8 +38,8 @@ def rectangular(x: float | Array[float], x0: float, w: float) -> Array[float]:
     """Rectangular distribution with position `x0` and unit intensity.
 
     Params:
-        x0: float - position;
-        w: float - width (full width).
+        x0: float - position
+        w: float - width (full width)
     """
 
     if isinstance(x, float):  # TODO: don't remove! It's for integrate.quad functions!
@@ -60,9 +60,8 @@ def voigt(x: float | Array[float], x0: float, sigma: float, gamma: float) -> Arr
 
     Params:
         x0: float - position;
-        sigma: float - width of Gaussian shape;
-        gamma: float - width of Lorentzian shape.
-
+        sigma: float - width of Gaussian shape
+        gamma: float - width of Lorentzian shape
     """
     rx = (x[-1] - x[0]) / 2
 
@@ -77,17 +76,17 @@ def pvoigt(x: float | Array[float], x0: float, w: float, a: float, r: float) -> 
     """Pseudo-Voigt distribution with position `x0` and unit intensity.
 
     Params:
-        x0: float - position;
-        w: float - width (full width at half maximum);
-        a: float - assymetry;
-        r: float - ratio (in range 0-1).
+        x0: float - position
+        w: float - width (full width at half maximum)
+        a: float - assymetry
+        r: float - ratio (in range [0; 1])
 
     A simple asymmetric line shape shape for fitting infrared absorption spectra.
     Aaron L. Stancik, Eric B. Brauns
     https://www.sciencedirect.com/science/article/abs/pii/S0924203108000453
     """
-
     sigma = 2*w / (1 + np.exp(a*(x - x0)) )
+
     G = np.sqrt(4*np.log(2)/np.pi) / sigma * np.exp(-4*np.log(2)*((x - x0)/sigma)**2)
     L = 2/np.pi/sigma/(1 + 4*((x - x0)/sigma)**2)
     F = r*L + (1 - r)*G
