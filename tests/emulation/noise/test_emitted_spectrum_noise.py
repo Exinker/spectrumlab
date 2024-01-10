acf837ad36bc1ec65902f7f6bbcbc97adf45f8e5
Pavel Vashchenko <exinker@gmail.com>
2024-01-10 22:07:37 +0700

    refac: split tests to different modules

--- tests/emulation/noise/test_emitted_spectrum_noise.py
+++ tests/emulation/noise/test_emitted_spectrum_noise.py
@@ -1,14 +1,10 @@
-import os
-
 import numpy as np
-import matplotlib.pyplot as plt
 import pytest
 
 from spectrumlab.alias import Electron, Percent
-from spectrumlab.emulation.concentration_calibration import EmittedExperimentConfigNaive as ExperimentConfig
 from spectrumlab.emulation.detector.linear_array_detector import Detector
-from spectrumlab.emulation.emulation import emulate_emitted_spectrum, emulate_absorbed_spectrum
-from spectrumlab.emulation.noise import EmittedSpectrumNoise, AbsorbedSpectrumNoise
+from spectrumlab.emulation.emulation import emulate_emitted_spectrum
+from spectrumlab.emulation.noise import EmittedSpectrumNoise
 
 
 @pytest.mark.parametrize(
@@ -91,46 +87,3 @@ def test_emulate_emitted_spectrum_noise(detector: Detector):
 
     assert np.all((emulated_noise - theoretical_noise)/theoretical_noise < tolerance)  # relative deviation
 
-
-@pytest.mark.parametrize(
-    'detector',
-    [detector for detector in Detector],
-)
-def test_emulate_absorbed_spectrum_noise(detector: Detector):
-    tolerance = 1e-1
-    n_times = 10_000
-    n_numbers = detector.config.n_pixels
-    base_level = 100
-    base_noise = EmittedSpectrumNoise(
-        detector=detector,
-        n_frames=1,
-    )
-
-    number = np.arange(n_numbers)
-    absorbance = np.logspace(-3, np.log10(3), n_numbers)
-
-
-    theoretical_noise = AbsorbedSpectrumNoise(
-        detector=detector,
-        n_frames=1,
-        base_level=base_level,
-        base_noise=base_noise,
-    )(absorbance)
-
-    spectrum = emulate_absorbed_spectrum(
-        intensity=np.array([base_level * 10**(-absorbance)]*n_times),
-        number=number,
-        noise=EmittedSpectrumNoise(
-            detector=detector,
-            n_frames=1,
-        ),
-        base_level=base_level,
-        base_noise=base_noise,
-        detector=detector,
-
-        is_clipped=False,
-    )
-    emulated_noise = np.std(spectrum.intensity, ddof=1, axis=0)
-
-    mask = ~np.isnan(emulated_noise)
-    assert np.all((emulated_noise[mask] - theoretical_noise[mask])/theoretical_noise[mask] < tolerance)  # relative deviation
