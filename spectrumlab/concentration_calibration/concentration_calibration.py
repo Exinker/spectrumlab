214613cbcc046be130356ad8e1df0e0a4eacf81e
Exinker <exinker@gmail.com>
2024-02-03 19:27:40 +0700

    feat: add calculate_intensity_handler

--- spectrumlab/concentration_calibration/concentration_calibration.py
+++ spectrumlab/concentration_calibration/concentration_calibration.py
@@ -1,5 +1,6 @@
 import os
 from abc import ABC, abstractmethod, abstractproperty
+from functools import partial
 from typing import Callable, Literal
 
 import numpy as np
@@ -7,7 +8,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 from spectrumlab.alias import Frame, Series
+from spectrumlab.emulation.noise import Noise
 from spectrumlab.emulation.spectrum import Spectrum
+from spectrumlab.line import Line
+from spectrumlab.peak.analyte_peak import gather_analyte_peak, GatherAnalytePeakConfig
 from spectrumlab.picture.config import COLOR, ALPHA
 
 from .exceptions import FitError
@@ -108,7 +112,7 @@ class ConcentrationCalibration(BaseConcentrationCalibration):
             print('blank: min concentration of data is not zero!')  # FIXME: add exception!
             index = self.data.index[self.data['concentration'] == min(self.data['concentration'])]
 
-        return self.data[index]
+        return self.data.loc[index]
 
     @property
     def coeff(self) -> tuple[Intercept, Slope]:
@@ -342,6 +346,17 @@ class ConcentrationCalibration(BaseConcentrationCalibration):
 
 
 # --------        handlers        --------
+def calculate_intensity_handler(line: Line, noise: Noise, config: GatherAnalytePeakConfig, verbose: bool = False, show: bool = False):
+    handler = partial(gather_analyte_peak, line=line, noise=noise, config=config, verbose=verbose, show=show)
+
+    def inner(spectrum: Spectrum) -> float:
+        peak = handler(spectrum=spectrum)
+
+        return peak.intensity
+
+    return inner
+
+
 def calibrate(spectra: Frame, handler: Callable[[Spectrum], float], show: bool = False) -> ConcentrationCalibration:
 
     # blank
