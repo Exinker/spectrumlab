275ff232c24fa0f9145d41afb9a8e4f6d89ead00
Exinker <exinker@gmail.com>
2024-02-03 14:45:22 +0700

    feat: add BaseWavelengthCalibration and WavelengthCalibration

--- spectrumlab/wavelength_calibration/wavelength_calibration.py
+++ spectrumlab/wavelength_calibration/wavelength_calibration.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 from typing import Callable
 
 import numpy as np
@@ -5,7 +6,49 @@ import numpy as np
 from spectrumlab.alias import Array, Number, NanoMeter
 from spectrumlab.spectrum import Spectrum
 
+from .exceptions import FitError
 
+
+class BaseWavelengthCalibration(ABC):
+
+    def __init__(self, deg: int) -> None:
+        self._deg = deg
+
+        self._coeff = None
+
+    @property
+    def deg(self) -> int:
+        return self._deg
+
+    @property
+    def coeff(self) -> Array[float]:
+        if self._coeff is None:
+            raise FitError('Fit the calibration before!')
+
+        return self._coeff
+
+    # --------        handlers        --------
+    @abstractmethod
+    def fit(self, number: Array[Number], wavelength: Array[NanoMeter]) -> 'BaseWavelengthCalibration':
+        raise NotImplementedError
+
+    def predict(self, number: Number | Array[Number]) -> Array[NanoMeter]:
+        return np.polyval(self.coeff, number)
+
+
+class WavelengthCalibration(BaseWavelengthCalibration):
+    
+    def __init__(self, deg: int) -> None:
+        super().__init__(deg=deg)
+
+    # --------        handlers        --------
+    def fit(self, number: Array[Number], wavelength: Array[NanoMeter]) -> 'WavelengthCalibration':
+        self._coeff = np.polyfit(number, wavelength, deg=self.deg)
+
+        return self
+
+
+# --------        handlers        --------
 def interpolate(spectrum: Spectrum, deg: int = 2) -> Callable[[Array[Number]], Array[NanoMeter]]:
     p = np.polyfit(spectrum.number, spectrum.wavelength, deg=deg)
 
@@ -13,3 +56,12 @@ def interpolate(spectrum: Spectrum, deg: int = 2) -> Callable[[Array[Number]], A
         return np.polyval(p, x)
 
     return inner
+
+
+def calibrate(spectrum: Spectrum, deg: int = 2) -> WavelengthCalibration:
+    return WavelengthCalibration(
+        deg=deg,
+    ).fit(
+        number=spectrum.number,
+        wavelength=spectrum.wavelength,
+    )
