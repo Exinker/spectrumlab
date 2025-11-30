a076558ade41824206ee453539ade8781f4c2515
Pavel Vashchenko <exinker@gmail.com>
2025-11-30 18:13:29 +0700

    Develop (#12)
    ## Tests
    * Remove tests to spectrumlab-emulation
    ## Refac
    * Update intensity regression transformer

--- src/spectrumlab/calibrators/concentration_calibrators/intensity_transformers/__init__.py
+++
@@ -1,7 +0,0 @@
-from .katskov_transformer import KatskovIntensityTransformer
-from .regression_transformer import RegressionIntensityTransformer
-
-__all__ = [
-    RegressionIntensityTransformer,
-    KatskovIntensityTransformer,
-]
